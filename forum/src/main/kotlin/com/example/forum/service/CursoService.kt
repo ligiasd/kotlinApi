5b01d8ae5966a8164c7d851c0e1919fba1f82d38
Ligiane Alves Souza Domingos <ligiane.alves.souza@gmail.com>
2021-08-09 19:39:38 -0300

    add transactional references p/ package controller, private val repository(package service)

--- forum/src/main/kotlin/com/example/forum/service/CursoService.kt
+++ forum/src/main/kotlin/com/example/forum/service/CursoService.kt
@@ -1,26 +1,15 @@
 package com.example.forum.service
 
 import com.example.forum.model.Curso
+import com.example.forum.repository.CursoRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class CursoService(var cursos: List<Curso> ) {
-
-    init {
-        val curso = Curso(
-            id = 1,
-            nome = "kotlin",
-            categorias = "Programação"
-
-        )
-        cursos = Arrays.asList(curso)
-    }
+class CursoService(private val repository : CursoRepository) {
 
     fun buscarPorId(id: Long): Curso {
-        return cursos.stream().filter({
-            c -> c.id == id
-        }).findFirst().get()
+        return repository.getOne(id)
     }
 
 }
--- forum/src/main/kotlin/com/example/forum/service/TopicoService.kt
+++ forum/src/main/kotlin/com/example/forum/service/TopicoService.kt
@@ -7,13 +7,15 @@ import com.example.forum.exception.NotFoundException
 import com.example.forum.mapper.TopicoFormMapper
 import com.example.forum.mapper.TopicoViewMapper
 import com.example.forum.model.Topico
+import com.example.forum.repository.TopicoRepository
+import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import java.util.stream.Collectors
 import kotlin.collections.ArrayList
 
 @Service
 class TopicoService(
-    private var topicos: List<Topico> = ArrayList(),
+    private val repository: TopicoRepository,
     private val topicoViewMapper: TopicoViewMapper,
     private val topicoFormMapper: TopicoFormMapper,
     private val notFoundMessage: String = "Topico não encontrado!"
@@ -21,48 +23,33 @@ class TopicoService(
 {
 
     fun listar(): List<TopicoView> {
-        return topicos.stream().map { t ->
+        return repository.findAll().stream().map { t ->
             topicoViewMapper.map(t)
         }.collect(Collectors.toList())
     }
 
     fun buscarPorId(id: Long): TopicoView {
-        val topico = topicos.stream().filter { t ->
-            t.id == id
-        }.findFirst().orElseThrow{NotFoundException(notFoundMessage)}
+        val topico = repository.findById(id).orElseThrow{NotFoundException(notFoundMessage)}
         return topicoViewMapper.map(topico)
     }
 
     fun cadastrar(form: NovoTopicoForm): TopicoView {
         val topico = topicoFormMapper.map(form)
-        topico.id = topicos.size.toLong() + 1
-        topicos = topicos.plus(topico)
+        repository.save(topico)
         return topicoViewMapper.map(topico)
     }
 
     fun atualizar(form: AtualizacaoTopicoForm) : TopicoView{
-        val topico = topicos.stream().filter{ t ->
-            t.id == form.id
-        }.findFirst().orElseThrow{NotFoundException(notFoundMessage)}
-        val topicoAtualizado = Topico(
-            id = form.id,
-            titulo = form.titulo,
-            mensagem = form.mensagem,
-            autor = topico.autor,
-            curso = topico.curso,
-            respostas = topico.respostas,
-            status = topico.status,
-            dataCriacao = topico.dataCriacao
-        )
-        topicos =  topicos.minus(topico).plus(topicoAtualizado)
-        return topicoViewMapper.map(topicoAtualizado)
+        val topico = repository.findById(form.id)
+            .orElseThrow{NotFoundException(notFoundMessage)}
+        topico.titulo = form.titulo
+        topico.mensagem = form.mensagem
+
+        return topicoViewMapper.map(topico)
     }
 
     fun deletar(id: Long) {
-        val topico = topicos.stream().filter{ t ->
-            t.id == id
-        }.findFirst().orElseThrow{NotFoundException(notFoundMessage)}
-        topicos =  topicos.minus(topico)
+        repository.deleteById(id)
 
     }
 
